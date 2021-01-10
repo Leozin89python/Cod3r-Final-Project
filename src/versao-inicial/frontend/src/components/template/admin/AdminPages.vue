a67b0e4650868f62bf521d2b9ba1ba9ee3c07d8f
Leozin89Python <leozin069@gmail.com>
2021-01-10 02:27:48 -0300

    Cod3r-Final-Project

--- src/versao-inicial/frontend/src/components/template/admin/AdminPages.vue
+++ src/versao-inicial/frontend/src/components/template/admin/AdminPages.vue
@@ -3,6 +3,22 @@
         <PageTitle icon="fa fa-cogs" main="Administração do Sistema"
          sub="Cadastro & Cia"
         />
+        <div class="admin-pages-tabs"> 
+            <b-card no-body>
+                <b-tabs card>
+                     <b-tab title="Artigos" active>
+                         <ArticleAdmin />
+                     </b-tab>
+
+                     <b-tab title="Categorias">
+                         <CategoryAdmin />
+                     </b-tab>
+                     <b-tab title="Usuários">
+                         <UserAdmin />
+                     </b-tab>
+                </b-tabs>
+            </b-card >
+        </div>
     </div>
 </template>
 
@@ -11,10 +27,14 @@
 
 
 <script>
+    import ArticleAdmin from './ArticleAdmin'
+    import CategoryAdmin from './CategoryAdmin'
+    import UserAdmin from './UserAdmin'
+
     import PageTitle from '../PageTitle'
     export default{
         name:'AdminPages',
-        components:{ PageTitle }
+        components:{ PageTitle ,ArticleAdmin ,CategoryAdmin ,UserAdmin }
     }
 </script>
 
---
+++ src/versao-inicial/frontend/src/components/template/admin/CategoryAdmin.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="category-admin">
+        <h1>Categoria Componete</h1>
+    </div>
+</template>
+
+
+
+
+
+<script>
+    export default{
+        name:'CategoryAdmin'
+    }
+</script>
+
+
+
+
+
+<style>
+    
+</style>
